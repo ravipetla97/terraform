36d331510d7212a54bc1430726a21e7e9ac274a5
ravipetla97 <raviatjnv@gmail.com>
2023-09-17 10:27:01 +0530

    aws ec2 creation and destroy

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+
+    }
+  }
+
+
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}
+
+resource "aws_instance" "terraform_test" {
+  ami           = "ami-0989fb15ce71ba39e"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "tetsing infra building with terraform"
+  }
+}
